a6b4e65a306e4c7f6b4172433c57e9857c149b1f
Sanjana Swami <sn696414@dal.ca>
2020-03-06 15:39:52 -0400

    arcs added, collision detection improved

--- tutorial/script.js
+++ tutorial/script.js
@@ -4,8 +4,11 @@ var context = canvas.getContext('2d');
 var ball = document.getElementById('ball');
 
 //canvas attributes
-var width = 300;
-var height = 300;
+var width = screen.width-20;
+var height = 600;
+
+canvas.width = width;
+canvas.height = height;
 
 //drawing shapes
 context.fillStyle = 'teal';
@@ -24,27 +27,40 @@ context.closePath();
 
 context.clearRect(0,0,width,height);
 
+//arcs
+context.beginPath();
+context.arc(width/2,height/2,100,Math.PI*2, false);
+context.stroke();
+context.closePath();
+
+context.clearRect(0,0,width,height);
+
 //animation
 var a = 100;
 var b = 100;
-var direction = 2;
+var size = 40;
+var directionX = 10;
+var directionY = 10;
 
 function drawCircle() {
 
-  if(a+40 == width || a==0)
+  context.drawImage(ball, a, b, size, size);
+
+  a = a + directionX;
+  b = b + directionY;
+
+  if(a+size > width || a==0)
   {
-    direction = direction * -1;
+    directionX = directionX * -1;
   }
-
-  a = a + direction;
-  //b = b + direction;
-  context.drawImage(ball, a, b, 40, 40);
+  else if (b+size > height || (b<0)) {
+    directionY = directionY * -1;
+  }
 }
-
 
 function update() {
 
-  context.clearRect(0,0,300,300);
+  context.clearRect(0,0,width,height);
 
   drawCircle();
 
